7a8b19fc93b7f7e86a4f859a7c76a2376c2891d2
Anh <qanh171002@gmail.com>
2024-10-26 17:03:35 +0700

    change color, background, add shop name

--- src/pages/login/LogInForm.jsx
+++ src/pages/login/LogInForm.jsx
@@ -3,40 +3,43 @@ import SocialLogIn from "../../components/SocialLogIn";
 
 function LogInForm() {
   return (
-    <div className="login-container">
-      <h2 className="form-title">Đăng nhập</h2>
+    <>
+      <h1>GreenFood</h1>
+      <div className="login-container">
+        <h2 className="form-title">Đăng nhập</h2>
 
-      <form action="#" className="login-form">
-        <InputField
-          type="email"
-          placeholder="Email/Số điện thoại/Tên đăng nhập"
-          icon="mail"
-        ></InputField>
-        <InputField
-          type="password"
-          placeholder="Mật khẩu"
-          icon="lock"
-        ></InputField>
-        {/* eslint-disable-next-line */}
-        <a href="#" className="forgot-password-link">
-          Quên mật khẩu
-        </a>
-        <button type="submit" className="login-button">
-          Đăng nhập
-        </button>
-        <p className="separator">
-          <span>or</span>
+        <form action="#" className="login-form">
+          <InputField
+            type="email"
+            placeholder="Email/Số điện thoại/Tên đăng nhập"
+            icon="mail"
+          ></InputField>
+          <InputField
+            type="password"
+            placeholder="Mật khẩu"
+            icon="lock"
+          ></InputField>
+          {/* eslint-disable-next-line */}
+          <a href="#" className="forgot-password-link">
+            Quên mật khẩu
+          </a>
+          <button type="submit" className="login-button">
+            Đăng nhập
+          </button>
+          <p className="separator">
+            <span>Hoặc</span>
+          </p>
+          <SocialLogIn />
+        </form>
+        <p className="signup-prompt">
+          Chưa có tài khoản?
+          {/* eslint-disable-next-line */}
+          <a href="#" className="signup-link">
+            Đăng ký
+          </a>
         </p>
-        <SocialLogIn />
-      </form>
-      <p className="signup-prompt">
-        Chưa có tài khoản?
-        {/* eslint-disable-next-line */}
-        <a href="#" className="signup-link">
-          Đăng ký
-        </a>
-      </p>
-    </div>
+      </div>
+    </>
   );
 }
 
--- src/pages/signup/SignUpForm.jsx
+++ src/pages/signup/SignUpForm.jsx
@@ -2,45 +2,56 @@ import InputField from "../../components/InputField";
 import SocialLogIn from "../../components/SocialLogIn";
 function SignUpForm() {
   return (
-    <div className="login-container">
-      <h2 className="form-title">Đăng ký</h2>
+    <>
+      <h1>GreenFood</h1>
+      <div className="login-container">
+        <h2 className="form-title">Đăng ký</h2>
 
-      <form action="#" className="login-form">
-        <InputField
-          type="text"
-          placeholder="Tên người dùng"
-          icon="person"
-        ></InputField>
-        <InputField
-          type="email"
-          placeholder="Email/Số điện thoại/Tên đăng nhập"
-          icon="mail"
-        ></InputField>
-        <InputField
-          type="password"
-          placeholder="Mật khẩu"
-          icon="lock"
-        ></InputField>
-        {/* eslint-disable-next-line */}
-        <a href="#" className="forgot-password-link">
-          Quên mật khẩu
-        </a>
-        <button type="submit" className="login-button">
-          Đăng ký
-        </button>
-        <p className="separator">
-          <span>or</span>
+        <form action="#" className="login-form">
+          <InputField
+            type="text"
+            placeholder="Tên người dùng"
+            icon="person"
+          ></InputField>
+          <InputField
+            type="email"
+            placeholder="Email/Số điện thoại/Tên đăng nhập"
+            icon="mail"
+          ></InputField>
+          <InputField
+            type="password"
+            placeholder="Mật khẩu"
+            icon="lock"
+          ></InputField>
+          <button type="submit" className="login-button">
+            Đăng ký
+          </button>
+          <p className="separator">
+            <span>Hoặc</span>
+          </p>
+          <SocialLogIn />
+        </form>
+        <p className="policy-prompt">
+          Bằng việc đăng ký, bạn đã đồng ý về
+          {/* eslint-disable-next-line */}{" "}
+          <a href="#" className="policy-link">
+            Điều khoản dịch vụ
+          </a>
+          {/* eslint-disable-next-line */} &{" "}
+          <a href="#" className="policy-link">
+            Chính sách bảo mật
+          </a>
         </p>
-        <SocialLogIn />
-      </form>
-      <p className="signup-prompt">
-        Chưa có tài khoản?
-        {/* eslint-disable-next-line */}
-        <a href="#" className="signup-link">
-          Đăng ký
-        </a>
-      </p>
-    </div>
+
+        <p className="signup-prompt">
+          Bạn đã có tài khoản?
+          {/* eslint-disable-next-line */}
+          <a href="#" className="signup-link">
+            Đăng nhập
+          </a>
+        </p>
+      </div>
+    </>
   );
 }
 
