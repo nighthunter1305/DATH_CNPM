0f9b58d8bb19328c8435b40278db96ddede97fc7
nighthunter1305 <mthanhdat1305@gmail.com>
2024-11-16 21:02:25 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 /* eslint-disable no-console */
-import express from "express";
-import { env } from "~/config/env";
-import { API } from "~/routes/index";
-import { connectToDB } from "~/config/connectDatabase";
+import express from 'express';
+import { env } from '~/config/env';
+import { API } from '~/routes/index';
+import { connectToDB } from '~/config/connectDatabase';
+import cors from 'cors';
 
 const SERVER = () => {
   const app = express();
 
   app.use(express.json());
-  app.use("/api", API);
+  app.use(cors());
+  app.use('/api', API);
 
-  if (env.BUILD_MODE == "dev") {
+  if (env.BUILD_MODE == 'dev') {
     app.listen(env.APP_PORT, () => {
       console.log(`Running on: http://${env.APP_HOST}:${env.APP_PORT}/api/home`);
     });
@@ -18,6 +20,6 @@ const SERVER = () => {
 };
 
 connectToDB()
-  .then(() => console.log("Connect to MySQL successfully!"))
+  .then(() => console.log('Connect to MySQL successfully!'))
   .then(() => SERVER())
   .catch((err) => console.log(err));
