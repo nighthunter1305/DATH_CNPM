0f02be94119d0cba352469268ff8dacac5f55f86
nighthunter1305 <mthanhdat1305@gmail.com>
2024-12-02 00:58:43 +0700

    Update adminController.js

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product.model');
+const { ProductModel } = require('../models/productmodel');
 const User = require('../models/user.model');
 
 // Xóa sản phẩm theo ID
@@ -6,19 +6,14 @@ exports.deleteProduct = async (req, res) => {
     try {
         const productId = req.params.id;
 
-        // Tìm và xóa sản phẩm
-        const product = await Product.findByIdAndDelete(productId);
-
-        if (!product) {
-            return res.status(404).json({ message: 'Product not found' });
-        }
+        // Sử dụng hàm từ productmodel.js
+        await ProductModel.deleteProduct(productId);
 
         return res.status(200).json({ message: 'Product deleted successfully' });
     } catch (error) {
         return res.status(500).json({ message: 'Internal server error', error });
     }
 };
-
 // Cấm người dùng theo ID
 exports.banUser = async (req, res) => {
     try {
