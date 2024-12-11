9ca50fd722d1d551af45b841e716dbafa1c3a03d
HuyHaloed <152784240+HuyHaloed@users.noreply.github.com>
2024-12-11 23:23:15 +0700

    Deploy FE v3

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
-const API_ROOT = process.env.REACT_APP_API_ROOT;
+import { env } from '../config/env';
+const API_ROOT = env.REACT_APP_API_ROOT;
 export const axiosInstance = axios.create({
   baseURL: API_ROOT,
   withCredentials: true
